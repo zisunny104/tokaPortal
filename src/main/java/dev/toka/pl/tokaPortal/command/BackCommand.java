9e34783615c331aced5d4d90c697b57e95db4988
Ethan940114 <ethan940114@gmail.com>
2021-06-13 22:11:44 +0800

    簡化back的code

--- src/main/java/dev/toka/pl/tokaPortal/command/BackCommand.java
+++ src/main/java/dev/toka/pl/tokaPortal/command/BackCommand.java
@@ -3,7 +3,8 @@ package dev.toka.pl.tokaPortal.command;
 import cn.nukkit.Player;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
-import dev.toka.pl.tokaPortal.Portal;
+
+import static dev.toka.pl.tokaPortal.Portal.backToLastPortalLocation;
 
 public class BackCommand extends Command {
     public BackCommand() {
@@ -11,29 +12,11 @@ public class BackCommand extends Command {
     }
 
     public boolean execute(CommandSender sender, String label, String[] args) {
-        if (sender.isPlayer()) {
-            Player player = (Player) sender;
-            Portal.backToLastPortalLocation(player);
-
-            /*//TODO:將要刪除
-            PlayerInfo pli = getPlayerInfo(player);
-            Location loc = getPlayerInfo(player).getDeathLocation();
-            if (loc != null) {
-                if (pli.getBossBarLength() >= 50) {
-                    callEvent(new PlayerPortalEvent(pli, "目前位置", "前次傳送位置", player.getLocation(), loc));
-                    player.teleport(loc);
-                    pli.sendText("§a[傳送]§b已返回前次傳送地點");
-                    pli.reBossBarLength();
-                } else {
-                    pli.sendText("§a[傳送]§b請等待冷卻時間結束後, \n再次進行傳送");
-                }
-            } else {
-                pli.sendText("§a[傳送]§b請先進行§e傳送§b(?");
-            }*/
+        if (sender instanceof Player) {
+            backToLastPortalLocation((Player) sender);
         } else {
             sender.sendMessage("§a[傳送]§e請在遊戲內進行§r");
         }
-
         return false;
     }
 }
